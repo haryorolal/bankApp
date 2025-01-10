0d9070465e5a252b0fe3b13793db0e502bbf9c97
Ayodele Olaleye <haryorolal2@gmail.com>
2025-01-10 13:58:53 +0100

    bank app

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Billing from "./Billing";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+
+export {
+  Billing,
+  Hero,
+  Navbar,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+};
